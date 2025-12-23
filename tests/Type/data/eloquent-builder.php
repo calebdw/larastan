78e79c91bab8b629b1f3b907d14152966939caa3
Can Vural <can9119@gmail.com>
2025-12-23 19:31:43 +0100

    fix tests

--- tests/Type/data/eloquent-builder.php
+++ tests/Type/data/eloquent-builder.php
@@ -276,14 +276,6 @@ function test(
         ->toBase()
     );
     assertType(
-        'stdClass|null',
-        User::getQuery()
-        ->select('some_model.created')
-        ->where('some_model.some_column', '=', true)
-        ->orderBy('some_model.created', 'desc')
-        ->first()
-    );
-    assertType(
         'Illuminate\Database\Query\Builder',
         User::query()
         ->whereNotBetween('a', [1, 5])
